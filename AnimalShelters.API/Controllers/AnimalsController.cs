cf05436bc773d123ab3c618d47a3f281af8a47b9
Daria Lepa <daria@pwrs.pl>
2018-08-28 22:58:51 +0200

    Animal controller route

--- AnimalShelters.API/Controllers/AnimalsController.cs
+++ AnimalShelters.API/Controllers/AnimalsController.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace AnimalShelters.API.Controllers
 {
+    [Route("api/[controller]")]
     public class AnimalsController : Controller
     {
         IAnimalRepository _animalRepository;
